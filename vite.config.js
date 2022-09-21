cfba253b9c2d48b25148b2d337ea4c6afb280a4e
DESKTOP-E2NI4HM\kwangsu <asia_prince@kakao.com>
2022-09-22 01:11:52 +0900

    no message

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
+
+/** @type {import('vite').UserConfig} */
+const config = {
+	plugins: [sveltekit()],
+	resolve: {
+		alias: {
+			$components: path.resolve('./srv/components'),
+			$assets: path.resolve('./src/assets')
+		}
+	}
+};
+
+export default config;
