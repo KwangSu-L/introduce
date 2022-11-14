ff6ebb3382fd56a5cbf2dba131f14564c971a7bb
DESKTOP-E2NI4HM\kwangsu <asia_prince@kakao.com>
2022-11-14 22:53:52 +0900

    no message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -769,14 +769,14 @@
 
 		<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
 			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-				<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-					<p class="w-full text-gray-600 text-xs md:text-sm px-6 mt-6">GETTING STARTED</p>
-					<div class="w-full font-bold text-xl text-gray-800 px-6">Lorem ipsum dolor sit amet.</div>
-					<p class="text-gray-600 text-base px-6 mb-5">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit
-						amet ligula.
-					</p>
-				</a>
+				<!-- <a href="#" class="flex flex-wrap no-underline hover:no-underline"> -->
+				<p class="w-full text-gray-600 text-xs md:text-sm px-6 mt-6">PYTHON</p>
+				<div class="w-full font-bold text-xl text-gray-800 px-6">Lorem ipsum dolor sit amet.</div>
+				<p class="text-gray-600 text-base px-6 mb-5">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet
+					ligula.
+				</p>
+				<!-- </a> -->
 			</div>
 			<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
 				<div class="flex items-center justify-start">
