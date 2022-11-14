48f2dbe59f8165e1566c2fb9d20c4b110e8835b8
DESKTOP-E2NI4HM\kwangsu <asia_prince@kakao.com>
2022-11-14 23:03:57 +0900

    no message

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -769,7 +769,7 @@
 
 		<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
 			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-				<div class="w-full font-bold text-xl text-gray-800 px-6">PYTHON</div>
+				<div class="w-full font-bold text-xl text-gray-800 px-6">Python</div>
 				<p class="text-gray-600 text-base px-6 mb-5">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet
 					ligula.
@@ -785,21 +785,11 @@
 					ligula.
 				</p>
 			</div>
-			<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-				<div class="flex items-center justify-center">
-					<button
-						class="mx-auto lg:mx-0 hover:underline gradient2 text-gray-800 font-extrabold rounded my-6 py-4 px-8 shadow-lg"
-					>
-						Action
-					</button>
-				</div>
-			</div>
 		</div>
 
 		<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
 			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-				<p class="w-full text-gray-600 text-xs md:text-sm px-6 mt-6">GETTING STARTED</p>
-				<div class="w-full font-bold text-xl text-gray-800 px-6">Lorem ipsum dolor sit amet.</div>
+				<div class="w-full font-bold text-xl text-gray-800 px-6">Svelte</div>
 				<p class="text-gray-600 text-base px-6 mb-5">
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet
 					ligula.
@@ -808,65 +798,29 @@
 		</div>
 		<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
 			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-				<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-					<p class="w-full text-gray-600 text-xs md:text-sm px-6 mt-6">GETTING STARTED</p>
-					<div class="w-full font-bold text-xl text-gray-800 px-6">Lorem ipsum dolor sit amet.</div>
-					<p class="text-gray-600 text-base px-6 mb-5">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit
-						amet ligula.
-					</p>
-				</a>
-			</div>
-			<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-				<div class="flex items-center justify-end">
-					<button
-						class="mx-auto lg:mx-0 hover:underline gradient2 text-gray-800 font-extrabold rounded my-6 py-4 px-8 shadow-lg"
-					>
-						Action
-					</button>
-				</div>
+				<div class="w-full font-bold text-xl text-gray-800 px-6">Deep Learning</div>
+				<p class="text-gray-600 text-base px-6 mb-5">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet
+					ligula.
+				</p>
 			</div>
 		</div>
 		<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
 			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-				<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-					<p class="w-full text-gray-600 text-xs md:text-sm px-6 mt-6">GETTING STARTED</p>
-					<div class="w-full font-bold text-xl text-gray-800 px-6">Lorem ipsum dolor sit amet.</div>
-					<p class="text-gray-600 text-base px-6 mb-5">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit
-						amet ligula.
-					</p>
-				</a>
-			</div>
-			<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-				<div class="flex items-center justify-end">
-					<button
-						class="mx-auto lg:mx-0 hover:underline gradient2 text-gray-800 font-extrabold rounded my-6 py-4 px-8 shadow-lg"
-					>
-						Action
-					</button>
-				</div>
+				<div class="w-full font-bold text-xl text-gray-800 px-6">Java Script</div>
+				<p class="text-gray-600 text-base px-6 mb-5">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet
+					ligula.
+				</p>
 			</div>
 		</div>
 		<div class="w-full md:w-1/3 p-6 flex flex-col flex-grow flex-shrink">
 			<div class="flex-1 bg-white rounded-t rounded-b-none overflow-hidden shadow">
-				<a href="#" class="flex flex-wrap no-underline hover:no-underline">
-					<p class="w-full text-gray-600 text-xs md:text-sm px-6 mt-6">GETTING STARTED</p>
-					<div class="w-full font-bold text-xl text-gray-800 px-6">Lorem ipsum dolor sit amet.</div>
-					<p class="text-gray-600 text-base px-6 mb-5">
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit
-						amet ligula.
-					</p>
-				</a>
-			</div>
-			<div class="flex-none mt-auto bg-white rounded-b rounded-t-none overflow-hidden shadow p-6">
-				<div class="flex items-center justify-end">
-					<button
-						class="mx-auto lg:mx-0 hover:underline gradient2 text-gray-800 font-extrabold rounded my-6 py-4 px-8 shadow-lg"
-					>
-						Action
-					</button>
-				</div>
+				<div class="w-full font-bold text-xl text-gray-800 px-6">Node.js</div>
+				<p class="text-gray-600 text-base px-6 mb-5">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at ipsum eu nunc commodo posuere et sit amet
+					ligula.
+				</p>
 			</div>
 		</div>
 	</div>
